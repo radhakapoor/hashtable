075651e101702606e78ca6b4e462457b07975ccc
radha kapoor <radha@radhakapoor.net>
2014-10-23 13:30:28 -0400

    added check_load_factor, ability to enlarge hashtable

--- hash_table_classes.py
+++ hash_table_classes.py
@@ -1,42 +1,39 @@
 
 
-"""How to modify the code?"""
-# - you can initialize the table with whatever size you want
-# - upon 'update', hashtable will automatically resize (enlarge or shrink)
-# - load factor = total number of keys / size
-
 class Hashtable(object):
 
-	hashtable = []	
+	hashtable = []
+	total_keys = 0
+	empty_buckets = 0
 
 	def __init__(self, name, size):
 		self.name = name
 		self.size = size
-
-		#can __init__ have methods? ie the growing and shrinking? 
-
-	def __getitem__(self, index):		
+		self.table = self.make_hashtable()
+		
+		
+	def __getitem__(self, index):
 		print self.hashtable[index]
 
 	def __repr__(self):
-		return "name of hashtable: {}, size of hashtable: {}".format(self.name, self.size)
+		return "name: {}, size: {}, draw hashtable: {}".format(self.name, self.size, self.table)
+
 
 	def make_hashtable(self):		
 		for i in range(0, self.size):
-		    self.hashtable.append([])	        
-		#print len(self.hashtable)
-		#print self.hashtable		
+			self.hashtable.append([])
 		return self.hashtable
 
 	def hash_string(self, key):
 		count = 0
-		for k in key:				
-			count = count + ord(k)	
-		allocated_bucket = count%self.size	
-		#print allocated_bucket
-		return allocated_bucket	
+		for k in key:
+			count = count + ord(k)
+		allocated_bucket = count%self.size
+		print "initial bucket is:"
+		print allocated_bucket		
+		return allocated_bucket
 
-	def hashtable_get_bucket(self, key):		
+	def hashtable_get_bucket(self, key):
 		bucket_number = self.hash_string(key)
 		return self.hashtable[bucket_number]
 
@@ -44,97 +41,90 @@ class Hashtable(object):
 		bucket = self.hashtable_get_bucket(key)
 		for entry in bucket:
 			if entry[0]==key:
-				entry[1] = value
+				entry[1]=value
 				return
 		bucket.append([key, value])
-		print bucket
+		self.total_keys = self.total_keys + 1		
+		return self.total_keys
 
+	def check_load(self):
+		load_factor = float(self.total_keys) / float(self.size)
+		if load_factor > 0.66:
+			print "enlarge table"
+			self.enlarge_hashtable()
+		if load_factor < 0.33:
+			print "shrink table"
+			self.shrink_hashtable()
+
+	def enlarge_hashtable(self):
+		existing_buckets = self.size		
+		self.size = self.size * 2	
+		add_new_buckets = self.size - existing_buckets		
+		for i in range(0, add_new_buckets):
+			self.hashtable.append([])		
+		self.re_allocate_buckets()			
+		return self.hashtable
+
+	def shrink_hashtable(self):
+		existing_buckets = self.size
+		self.size = self.size / 2
+		remove_buckets = existing_buckets - self.size
+		print remove_buckets
+		for i in range(0, remove_buckets):
+			self.hashtable.remove([])
+		self.re_allocate_buckets()
+		return self.hashtable
+		# for this to work need to figure out how to remove empty buckets
+		# this is adding key, value which are already existing in the			
+		
+	def re_allocate_buckets(self):
+		for bucket in self.hashtable:
+			for entry in bucket:
+				count = 0
+				for k in entry[0]:					
+					count = count + ord(k)					
+				new_bucket_number = count%self.size				
+				print "new_bucket_number is:"
+				print new_bucket_number				
+				get_new_bucket = self.hashtable[new_bucket_number]				
+		get_new_bucket.append([entry[0], entry[1]])
+		return get_new_bucket
+	
 	def hashtable_delete(self, key):
 		bucket = self.hashtable_get_bucket(key)
 		for entry in bucket:
 			if entry[0]==key:
 				bucket.remove(entry[:2])
-		print bucket
-		return
+		return 
 
 	def hashtable_lookup(self, key):
 		bucket = self.hashtable_get_bucket(key)
-		for entry in bucket:			
-			if entry[0] == key:
+		for entry in bucket:
+			if entry[0]==key:				
 				return entry[1]
-		return "{} is not a key in the hashtable".format(key)		
-		
-		
-"""initialize the hashtable"""
-phone_hashtable = Hashtable('phonenumbers', 10)
+		return "Error! {} is not a key in the hashtable".format(key)
 
-"""Get bucket number for 'Asha'"""
-print "Get bucket number for Asha:"
-print phone_hashtable.hash_string('asha')
+hashtable1 = Hashtable('test', 40)
 
-"""Create the empty hashtable"""
-print "Print the empty hashtable:"
-print phone_hashtable.make_hashtable()
+hashtable1.hashtable_insert('cat', 1)
+hashtable1.hashtable_insert('dog', 2)
+hashtable1.hashtable_insert('fish', 3)
+hashtable1.hashtable_insert('pigeon', 4)
+hashtable1.hashtable_insert('whale', 5)
+hashtable1.hashtable_insert('crocodile', 6)
+hashtable1.hashtable_insert('elephant', 7)
+hashtable1.hashtable_insert('lizard', 8)
+hashtable1.hashtable_insert('snake', 9)
+hashtable1.hashtable_insert('killer whale', 10)
+hashtable1.hashtable_insert('duck',11)
 
-"""Index the hashtable"""
-print "Index the hashtable:"
-phone_hashtable[2]
+print "hashtable without resizing:"
+print hashtable1.hashtable
 
-"""Get the bucket for keyword 'asha'"""
-print "Get the bucket for keyword 'asha':"
-print phone_hashtable.hashtable_get_bucket('asha')
+print hashtable1.check_load()
 
-"""Insert entries into the hashtable"""
-print "Insert entries into the hashtable:"
-phone_hashtable.hashtable_insert('asha', '0208 998 3443')
-phone_hashtable.hashtable_insert('radha', '646 641 7999')
-phone_hashtable.hashtable_insert('paresh', '0776 468 5109')
-
-"""Print the hashtable"""
-print "Print the hashtable"
-print phone_hashtable.hashtable
-
-"""Delete from the hashtable"""
-print "Delete from the hashtable:"
-phone_hashtable.hashtable_delete('radha')
-
-"""Print the hashtable"""
-print "Print the hashtable"
-print phone_hashtable.hashtable
-
-"""Look up 'suraj' in the hashtable"""
-print "Lookup 'suraj' in the hashtable:"
-print phone_hashtable.hashtable_lookup('suraj')
-
-"""Features of a good hashing function"""
-# Makes use of all info provided by key
-# Uniformly distributes output across table
-# Maps similar keys to very different hash values
-# Uses only very fast operations
-
-"""Dynamic Resizing"""
-# - A general purpose hashtable class will almost always have some ways to resize
-# and it is good practice to include even for simple custom tables. 
-# - An implementation should check the load factor and do something if it becomes too large. 
-# - load factor = number of entries / number of buckets (n/k)
-# - This needs to be done only on inserts since that is the only thing that would increase it. 
-# in Python dict, table size is resized when the load factor is greater than 2/3. 
-# - resizing is accompanied by a full or incremental table rehash whereby existing items are mapped to new bucket locations. 
-
-"""Shrinking"""
-# - when load factor falls below a second threshold, all entries are moved to a new smaller table. 
-# - should that load factor be 1/3? 
-
-"""Add ability to delete entries"""
-# - that is the only time when shrinking is relevant
-
-"""Whether to deal with collisions"""
-
-"""Whether to improve the hashing function"""
-
-"""This whole thing about prime numbers"""
-
-"""Test the hashtable by having it pick up words from a book chapter?"""
+print "hashtable with new buckets"
+print hashtable1.hashtable
 
 
 
