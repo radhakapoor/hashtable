510f31223f4ef7c5a9e2b32c51eac4b956190e2a
radha kapoor <radha@radhakapoor.net>
2014-11-07 14:01:57 -0500

    minor clean up

--- hash_table_classes.py
+++ hash_table_classes.py
@@ -14,6 +14,9 @@ class Hashtable(object):
 	def __repr__(self):
 		return "size: {}, draw hashtable: {}".format(self.size, self.hashtable)	
 
+	def __getitem__(self, index):
+		return self.hashtable[index]       
+
 	def hash_string(self, key):
 		count = 0		
 		for k in key:
@@ -25,7 +28,7 @@ class Hashtable(object):
 		bucket_number = self.hash_string(key)		
 		return self.hashtable[bucket_number]
 
-	def hashtable_find_bucket(self, bucket, key):
+	def hashtable_get_entry(self, bucket, key):
 		for entry in bucket:
 			if entry[0] == key:
 				return entry
@@ -33,7 +36,7 @@ class Hashtable(object):
 
 	def hashtable_insert(self, key, value):		
 		bucket = self.hashtable_get_bucket(key)
-		entry = self.hashtable_find_bucket(bucket, key)
+		entry = self.hashtable_get_entry(bucket, key)
 		if entry:
 			entry[1] = value
 			for each in self.all_entries:
@@ -45,10 +48,10 @@ class Hashtable(object):
 		self.all_entries.append([key, value]) 				
 		self.total_keys = self.total_keys + 1		
 		self.check_load()		
-		return self.total_keys	
+		return #self.total_keys	
 
 	def hashtable_lookup(self, key):
-		entry = self.hashtable_find_bucket(self.hashtable_get_bucket(key), key)
+		entry = self.hashtable_get_entry(self.hashtable_get_bucket(key), key)
 		if entry:					
 			return entry[1]			
 		else:
@@ -79,7 +82,7 @@ class Hashtable(object):
 		self.clear_buckets()		
 		for each in self.all_entries:
 			bucket = self.hashtable_get_bucket(each[0])
-			entry = self.hashtable_find_bucket(bucket, each[0])
+			entry = self.hashtable_get_entry(bucket, each[0])
 			bucket.append([each[0], each[1]])
 					
 	def shrink_hashtable(self):		
@@ -157,19 +160,7 @@ print hashtable1.hashtable_lookup('elephant')
 
 print hashtable1
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+print hashtable1.total_keys
 
 
 
